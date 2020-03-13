62f00d6c0a155c7b94087a0ef37c9b655ce26885
Daniel Russ <druss@nih.gov>
2020-03-13 09:13:52 -0400

    cleaned up UI

--- app.R
+++ app.R
@@ -5,18 +5,24 @@ ui <- fluidPage(
   # Input Function
 ##  sliderInput(inputId = "num",label = "Choose a number",value = 5,min = 1,max=10),
 ##  sliderInput(inputId = "num2",label = "Choose a second number",value = 5,min = 1,max=10),
-  h3("A shiny verions of Euclids",tags$a(href="https://www.rstudio.com", "Click here!")),
-  textInput(inputId = "url",label = "enter URL"),
+  h3("A shiny verions of Euclids",tags$a(href="https://github.com/danielruss/shinyEuclids", "source on github")),
+  textInput(inputId = "url",label = "enter URL",value = "https://episphere.github.io/qaqc/iris.csv"),
+  actionButton(inputId = "clicks", label = "calculate distance"),
   # Output Function
-  tableOutput(outputId = "myTable"),
-  textOutput(outputId = "meanValue",inline = F),
-  actionButton(inputId = "clicks", label = "calculate distance")
+  p(style="padding:4px",
+    textOutput(outputId = "distLabel",inline = T),
+    textOutput(outputId = "meanValue",inline = T)
+    ),
+  plotOutput(outputId = "myPlot")
   );
 
 server <- function(input, output){
   observeEvent(input$clicks,{ 
-    data <- read_csv(isolate(input$url)) %>% select_if(is.numeric) %>% drop_na()
-    output$meanValue <- renderText({mean(dist(as.matrix(data)))}) ;
+    output$distLabel = renderText("mean distance: ")
+    data <- read_csv(isolate(input$url)) %>% select_if(is.numeric) %>% drop_na();
+    d <- dist(as.matrix(data));
+    output$meanValue <- renderText({mean(d)})
+    output$myPlot <- renderPlot({heatmap(as.matrix(d))})
   });
  
 }
